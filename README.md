87d58c515a8e4e50d5f91e2aba655c49cee1eed2
Desmond Morris <hi@desmondmorris.com>
2013-12-05 14:44:55 -0500

    Adds terminus configuration options and pauth

--- README.md
+++ README.md
@@ -9,7 +9,16 @@ A wercker build step that installs Terminus, the Pantheon command line tool
 
 ## Options
 
-This step has no options.
+* `pantheon-email` (required) Pantheon user email address.
+* `pantheon-password` (required) Pantheon user password.
+* `pantheon-site-uuid` (required) Pantheon site UUID
+
+## Example
+
+    - terminus:
+        pantheon-email: $EMAIL
+        pantheon-password: $PASSWORD
+        pantheon-site-uuid: $UUID
 
 ## Contribute
 
--- run.sh
+++ run.sh
@@ -1,2 +1,22 @@
+#!/bin/sh
+set -e
+cd $HOME
+if [ ! -n "$WERCKER_TERMINUS_PANTHEON_EMAIL" ]
+then
+    fail 'missing email'
+fi
+
+if [ ! -n "$WERCKER_TERMINUS_PANTHEON_PASSWORD" ]
+then
+    fail 'missing password'
+fi
+
+if [ ! -n "$WERCKER_TERMINUS_PANTHEON_SITE_UUID" ]
+then
+    fail 'missing site uuid'
+fi
+
 composer create-project pantheon-systems/terminus $HOME/.drush/terminus -s dev --no-dev -n
 drush cc drush
+
+drush pauth $WERCKER_TERMINUS_PANTHEON_EMAIL --password=$WERCKER_TERMINUS_PANTHEON_PASSWORD
